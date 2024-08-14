4f58e9aeed9b0dd4555f198a4fb63798950695b2
Orche <orkv0099@gmail.com>
2024-08-14 11:32:18 +0300

    made the interaction code cleaner

--- scripts/characters/NPC.gd
+++ scripts/characters/NPC.gd
@@ -6,12 +6,10 @@ class_name NPC extends Area2D
 @export_category("Dialog Attributes")
 @export_file("*.ogg") var _dialog_voice = "res://sounds/dialog/sample2.ogg"
 @export_multiline var _dialog: Array[String]
-@export var is_interactable_with_player = false
 
 @onready var _interaction_area := $InteractionArea
 @onready var _dialog_index = 0
 @onready var textbox = preload("res://scenes/HUD/interaction/Textbox.tscn").instantiate()
-@onready var is_interacting_with_player = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,33 +18,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-#func _input(event):
-	#interact_with_player()
-	
-func interact_with_player():
-	if Input.is_action_just_pressed("INTERACT") and is_interactable_with_player and not textbox.reading_dialog:
-		if is_interacting_with_player:
-			_dialog_index = _dialog_index + 1
-			if _dialog_index >= _dialog.size():
-				is_interactable_with_player = false
-				textbox.queue_free()
-				return
-			
-			textbox.text = _dialog[_dialog_index]
-			
-		else:
-			is_interactable_with_player = true
-			_dialog_index = 0
-			textbox.text = _dialog[_dialog_index]
-			add_child(textbox)
-			
-			
-				
-func _on_body_entered(body):
-	if body is Player:
-		is_interactable_with_player = true
-
-func _on_body_exited(body):
-	if body is Player:
-		is_interactable_with_player = false
